4f1ab3a78e9ccd35829672930b66fc8dd316c969
Froznar <francisco.marin@ucsp.edu.pe>
2016-11-23 13:43:03 -0500

    Todo listo, eliminación de formularios con funciones que ocasionaban errores en momento de compilación

--- lib/component/user_administrator/index/user_administrator_management.dart
+++ lib/component/user_administrator/index/user_administrator_management.dart
@@ -1,34 +1,34 @@
-
+library logistic_ui.user_administrator_management;
 import 'package:angular2/angular2.dart';
 import 'package:angular2/router.dart';
 
 import 'package:logistic_ui/request.dart';
 import 'package:logistic_ui/providers.dart';
 import 'package:logistic_ui/model.dart';
-
 import 'package:logistic_ui/component/footer_panel.dart';
-import 'package:logistic_ui/component/user_administrator/user_administrator_modify/user_administrator_modify.dart';
-import 'package:logistic_ui/component/user_administrator/user_administrator_register/user_administrator_register.dart';
+import 'package:logistic_ui/component/user_administrator_management/user_administrator_modify/user_administrator_modify.dart';
+import 'package:logistic_ui/component/user_administrator_management/user_administrator_register/user_administrator_register.dart';
+
 
 @Component(
     selector: 'user-administrator-management',
     templateUrl: 'user_administrator_management.html',
     directives: const[ROUTER_DIRECTIVES, NgIf, NgFor,
-      UserAdministratorModify,UserAdministratorRegister,FooterPanel],
+      UserAdministratorRegister,UserAdministratorModify,FooterPanel],
     viewProviders: const [LOGISTIC_SERVICE_PROVIDERS])
 
-class UserAdministratorManagement implements AfterViewInit {
+class UserAdministratorManagement implements AfterViewInit{
 
   ApplicationService applicationService;
   ApplicationInfo applicationInfo = new ApplicationInfo(name: "Blazing Box");
   UserAdministratorManagement(ApplicationService this.applicationService);//, Router this.router);
   Router router;
 
-  @ViewChild(UserAdministratorModify)
-  UserAdministratorModify userAdministratorModify;
-
   @ViewChild(UserAdministratorRegister)
   UserAdministratorRegister userAdministratorRegister;
+
+  @ViewChild(UserAdministratorModify)
+  UserAdministratorModify userAdministratorModify;
 
   @ViewChild(FooterPanel)
   FooterPanel footerPanel;
@@ -45,5 +45,5 @@ class UserAdministratorManagement implements AfterViewInit {
   void onSelect(var navBar) {
     NavBar = navBar;
   }
-
 }
+
